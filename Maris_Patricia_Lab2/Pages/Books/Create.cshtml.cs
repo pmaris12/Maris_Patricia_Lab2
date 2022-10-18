cd493ab0aacf6c8051bfdeafd47dffdf813d165f
Maris Patricia <patryalin1122@gmail.com>
2022-10-18 17:12:28 +0300

    Add project files.

---
+++ Maris_Patricia_Lab2/Pages/Books/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Maris_Patricia_Lab2.Data;
+using Maris_Patricia_Lab2.Models;
+
+namespace Maris_Patricia_Lab2.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Maris_Patricia_Lab2.Data.Maris_Patricia_Lab2Context _context;
+
+        public CreateModel(Maris_Patricia_Lab2.Data.Maris_Patricia_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Maris_Patricia_Lab2/Pages/Publishers/Create.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Maris_Patricia_Lab2.Data;
+using Maris_Patricia_Lab2.Models;
+
+namespace Maris_Patricia_Lab2.Pages.Publishers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Maris_Patricia_Lab2.Data.Maris_Patricia_Lab2Context _context;
+
+        public CreateModel(Maris_Patricia_Lab2.Data.Maris_Patricia_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
+"PublisherName");
+            return Page();
+        }
+    
+
+
+        [BindProperty]
+        public Publisher Publisher { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Publisher.Add(Publisher);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
